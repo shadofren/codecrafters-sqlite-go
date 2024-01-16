8278d85b41a1d452ecab709a712d780bc0a72b16
Tran Manh Tu <xxlaguna93@gmail.com>
2024-01-16 22:10:22 +0800

    read page header to determine number of cells

--- app/main.go
+++ app/main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"bytes"
-	"encoding/binary"
 	"fmt"
-	"log"
 	"os"
 	// Available if you need it!
-	// "github.com/xwb1989/sqlparser"
-)
+	/* "github.com/xwb1989/sqlparser" */)
 
 // Usage: your_sqlite3.sh sample.db .dbinfo
 func main() {
@@ -17,26 +13,8 @@ func main() {
 
 	switch command {
 	case ".dbinfo":
-		databaseFile, err := os.Open(databaseFilePath)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		header := make([]byte, 100)
-
-		_, err = databaseFile.Read(header)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		var pageSize uint16
-		if err := binary.Read(bytes.NewReader(header[16:18]), binary.BigEndian, &pageSize); err != nil {
-			fmt.Println("Failed to read integer:", err)
-			return
-		}
-
-		// Uncomment this to pass the first stage
-		fmt.Printf("database page size: %v", pageSize)
+    dbinfo := DBInfoCmd(databaseFilePath)
+		fmt.Printf("database page size: %v", dbinfo.PageSize)
 	default:
 		fmt.Println("Unknown command", command)
 		os.Exit(1)
