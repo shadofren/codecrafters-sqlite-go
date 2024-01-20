b6070cb534bf114310fc35a29b6f65afd9713bbb
Tran Manh Tu <xxlaguna93@gmail.com>
2024-01-20 18:31:02 +0800

    table scan and then filter, TODO add filter during table scan

--- app/sqlite.go
+++ app/sqlite.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/xwb1989/sqlparser"
@@ -20,14 +21,18 @@ const (
 )
 
 const (
-	NULL  = 0x00
-	INT8  = 0x01
-	INT16 = 0x02
-	INT24 = 0x03
-	INT32 = 0x04
-	// TODO others
-	BLOB = 0xc
-	TEXT = 0xd
+	NULL    = 0x00
+	INT8    = 0x01
+	INT16   = 0x02
+	INT24   = 0x03
+	INT32   = 0x04
+	INT48   = 0x05
+	INT64   = 0x06
+	FLOAT64 = 0x07
+	ZERO    = 0x08
+	ONE     = 0x09
+	BLOB    = 0xc
+	TEXT    = 0xd
 )
 
 type DBInfo struct {
@@ -52,6 +57,11 @@ type BTreePageHeader struct {
 	StartOfCellContent          uint16
 	NumberOfFragmentedFreeBytes uint8
 	RightMostPointer            uint32
+}
+
+type InteriorTableCell struct {
+	LeftChildPage int64 // actually only 4 byte but to be consistent
+	RowId         int64 // varint
 }
 
 func DBInfoCmd(dbPath string) {
@@ -79,80 +89,84 @@ func DBSelectCmd(dbPath string, stmt *sqlparser.Select) {
 			continue
 		}
 
-		pageHeader, headerSize, pageContent := readPage(dbPath, info, table.RootPage)
-
+		isCount := false
 		outputCols := make([]string, 0)
 		/* should be an AliasedExpr, work on 1 column right now */
 		for _, expr := range stmt.SelectExprs {
 			expr := expr.(*sqlparser.AliasedExpr).Expr
 			if _, ok := expr.(*sqlparser.FuncExpr); ok {
-				fmt.Println(pageHeader.NumberOfCells)
-				return
+				isCount = true
+				break
 			}
 			colName := expr.(*sqlparser.ColName).Name.String()
 			outputCols = append(outputCols, colName)
 		}
 
+		_, cells := readPage(dbPath, info, table.RootPage)
+		if isCount {
+			fmt.Println(len(cells))
+			return
+		}
+
 		sql := strings.ReplaceAll(table.Sql, "autoincrement", "")
+		sql = strings.ReplaceAll(sql, "\"", "") // table name has doublequote
 		tableSql, err := sqlparser.Parse(sql)
 		must(err)
 		// the columns in order, then we parse the data
 		columns := tableSql.(*sqlparser.DDL).TableSpec.Columns
 		tableCols := make(map[string]int)
-    for i, col := range columns {
-      tableCols[col.Name.String()] = i
-    }
-		reader := bytes.NewReader(pageContent)
-		reader.Seek(int64(headerSize), io.SeekStart)
-		cellStarts := []int64{}
-		// start of cell pointer array
-		for i := 0; i < int(pageHeader.NumberOfCells); i++ {
-			var p uint16
-			binary.Read(reader, binary.BigEndian, &p)
-			cellStarts = append(cellStarts, int64(p))
+		for i, col := range columns {
+			tableCols[col.Name.String()] = i
 		}
 
-		rows := make([][]any, 0)
-		for _, idx := range cellStarts {
-			reader.Seek(idx, io.SeekStart)
-			row := readCell(reader)
-      if filter(row, tableCols, stmt.Where) {
-        rows = append(rows, row)
+    // filter the cells 
+    filteredCells := make([][]any, 0)
+    _ = filteredCells
+    for _, cell := range cells {
+      _ = cell
+      if filter(cell, tableCols, stmt.Where) {
+        filteredCells = append(filteredCells, cell)
       }
-		}
+    }
 
-    results := make([][]string, len(rows))
+		results := make([][]string, len(filteredCells))
 
 		for _, outCol := range outputCols {
 			if idx, ok := tableCols[outCol]; ok {
-				for i, row := range rows {
-          value := row[idx].(string)
-          results[i] = append(results[i], value)
+				for i, row := range filteredCells {
+					switch r := row[idx].(type) {
+					case int64:
+						results[i] = append(results[i], strconv.FormatInt(r, 10))
+					case string:
+						results[i] = append(results[i], r)
+          default:
+            fmt.Println("not supported type")
+					}
 				}
 			}
 		}
 
-    for _, res := range results {
-      fmt.Println(strings.Join(res, "|"))
-    }
+		for _, res := range results {
+			fmt.Println(strings.Join(res, "|"))
+		}
 	}
 }
 
 func filter(row []any, tableCols map[string]int, where *sqlparser.Where) bool {
-  if where == nil {
-    return true
-  }
+	if where == nil {
+		return true
+	}
 
-  if where, ok := where.Expr.(*sqlparser.ComparisonExpr); ok {
-    // assume to be equal
-    colName := where.Left.(*sqlparser.ColName).Name.String()
-    colValue := string(where.Right.(*sqlparser.SQLVal).Val)
-    if idx, ok := tableCols[colName]; ok {
-      value := row[idx]
-      return value == colValue
-    }
-  }
-  return false
+	if where, ok := where.Expr.(*sqlparser.ComparisonExpr); ok {
+		// assume to be equal
+		colName := where.Left.(*sqlparser.ColName).Name.String()
+		colValue := string(where.Right.(*sqlparser.SQLVal).Val)
+		if idx, ok := tableCols[colName]; ok {
+			value := row[idx]
+			return value == colValue
+		}
+	}
+	return false
 }
 
 func readInfo(dbPath string) (*DBInfo, *BTreePageHeader) {
@@ -191,7 +205,7 @@ func readInfo(dbPath string) (*DBInfo, *BTreePageHeader) {
 	tables := make([]*SQLiteSchema, 0)
 	for _, idx := range cellStarts {
 		reader.Seek(idx, io.SeekStart)
-		row := readCell(reader)
+		row := readTableLeafCell(reader)
 		table := &SQLiteSchema{}
 		table.Type, _ = row[0].(string)
 		table.Name, _ = row[1].(string)
@@ -205,7 +219,7 @@ func readInfo(dbPath string) (*DBInfo, *BTreePageHeader) {
 	return dbinfo, pageHeader
 }
 
-func readPage(dbPath string, dbinfo *DBInfo, page int64) (*BTreePageHeader, int, []byte) {
+func readPage(dbPath string, dbinfo *DBInfo, page int64) (*BTreePageHeader, [][]any) {
 	dbFile, err := os.Open(dbPath)
 	must(err)
 	defer dbFile.Close()
@@ -217,13 +231,53 @@ func readPage(dbPath string, dbinfo *DBInfo, page int64) (*BTreePageHeader, int,
 	dbFile.Read(content)
 
 	reader := bytes.NewReader(content)
-	pageHeader, headerSize := readPageHeader(reader)
+	pageHeader, _ := readPageHeader(reader)
 
-	return pageHeader, headerSize, content
+	cellStarts := []int64{}
+	// start of cell pointer array
+	for i := 0; i < int(pageHeader.NumberOfCells); i++ {
+		var p uint16
+		binary.Read(reader, binary.BigEndian, &p)
+		cellStarts = append(cellStarts, int64(p))
+	}
+	cells := make([][]any, 0)
+	switch pageHeader.PageType {
+	case LeafTablePage:
+		for _, offset := range cellStarts {
+			reader.Seek(offset, io.SeekStart)
+			cell := readTableLeafCell(reader)
+			cells = append(cells, cell)
+		}
+	case InteriorTablePage:
+		interiorCells := make([]*InteriorTableCell, 0)
+		for _, offset := range cellStarts {
+			reader.Seek(offset, io.SeekStart)
+			cell := readTableInteriorCell(reader)
+			interiorCells = append(interiorCells, cell)
+		}
+		// recursion to get the actual cell
+		for _, cell := range interiorCells {
+			_, rows := readPage(dbPath, dbinfo, cell.LeftChildPage)
+			cells = append(cells, rows...)
+		}
+	default:
+		fmt.Println("index not supported")
+	}
+	return pageHeader, cells
 }
 
-func readCell(reader *bytes.Reader) []any {
+func readTableInteriorCell(reader *bytes.Reader) *InteriorTableCell {
 
+	cell := &InteriorTableCell{}
+	var leftPage uint32 // 4 byte int
+	binary.Read(reader, binary.BigEndian, &leftPage)
+	cell.LeftChildPage = int64(leftPage)
+	rowId, _ := readVarint(reader)
+	cell.RowId = rowId
+	return cell
+}
+
+func readTableLeafCell(reader *bytes.Reader) []any {
 	_, _ = readVarint(reader)
 	rowId, _ := readVarint(reader)
 	totalHeaderSize, offset := readVarint(reader)
@@ -241,16 +295,29 @@ func readCell(reader *bytes.Reader) []any {
 		switch t {
 		case NULL:
 			// this is place holder for the row Id primary index?
-			data = append(data, int64(rowId))
+			data = append(data, nil)
 		case INT8:
 			v, _ := reader.ReadByte()
 			data = append(data, int64(v))
 		case INT16:
-			fmt.Println("INT16")
+			var v uint16
+			err := binary.Read(reader, binary.BigEndian, &v)
+			must(err)
+			data = append(data, int64(v))
 		case INT24:
 			fmt.Println("INT24")
 		case INT32:
 			fmt.Println("INT32")
+		case INT48:
+			fmt.Println("INT48")
+		case INT64:
+			fmt.Println("INT64")
+		case FLOAT64:
+			fmt.Println("FLOAT64")
+		case ZERO:
+			data = append(data, 0)
+		case ONE:
+			data = append(data, 1)
 		default:
 			if t&1 == 0 {
 				fmt.Println("a blob of size", (t-12)/2)
@@ -262,6 +329,7 @@ func readCell(reader *bytes.Reader) []any {
 			}
 		}
 	}
+  data[0] = rowId
 	return data
 }
 
